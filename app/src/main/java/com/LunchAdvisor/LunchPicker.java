254212c20427911f4a41c62ec5245ac4fa16c355
zanpicman <picman.zan@gmail.com>
2023-01-10 21:35:00 +0100

    Basic get request works for dishes

---
+++ app/src/main/java/com/LunchAdvisor/LunchPicker.java
@@ -0,0 +1,84 @@
+package com.LunchAdvisor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.content.Intent;
+
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class LunchPicker extends AppCompatActivity {
+    private RequestQueue requestQueue;
+    private TextView dishes;
+    private String url = "https://lunchadvisor1.azurewebsites.net/api/DishesApi";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lunch_picker);
+        setTitle("Lunch Picker");
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        dishes = (TextView) findViewById(R.id.osebe);
+    }
+
+    public void showDishes(View view) {
+        if (view != null) {
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            requestQueue.add(request);
+        }
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+
+        @Override
+        public void onResponse(JSONArray response) {
+            ArrayList<String> data = new ArrayList<>();
+            for (int i = 0; i < response.length(); i++) {
+                try {
+                    JSONObject dish = response.getJSONObject(i);
+                    int id = dish.getInt("id");
+                    int restaurantID = dish.getInt("restaurantID");
+                    String name = dish.getString("name");
+                    String imageURL = dish.getString("imageURL");
+                    data.add(id + " " + name);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                dishes.setText("");
+
+            for (String row : data) {
+                String currentText = dishes.getText().toString();
+                dishes.setText(currentText + "\n\n" + row);
+            }
+
+            }
+
+        }
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
+
+}
--- app/src/main/java/com/LunchAdvisor/MainActivity.java
+++ app/src/main/java/com/LunchAdvisor/MainActivity.java
@@ -2,6 +2,7 @@ package com.LunchAdvisor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.SettingInjectorService;
 import android.os.Bundle;
 
 import android.content.Intent;
@@ -9,70 +10,37 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.view.View;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RequestQueue requestQueue;
-    private TextView osebe;
-private String url ="https://university-dev.azurewebsites.net/api/v1/student";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        requestQueue = Volley.newRequestQueue(getApplicationContext());
-        osebe = (TextView) findViewById(R.id.osebe);
-    }
-    public void prikaziOsebe(View view){
-        if (view != null) {
-            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
-            requestQueue.add(request);
-
-        }
-
     }
 
-    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
-        @Override
-        public void onResponse(JSONArray response) {
-            ArrayList<String> data = new ArrayList<>();
-            for (int i = 0; i < response.length(); i++) {
-                try {
-                    JSONObject object = response.getJSONObject(i);
-                    String name = object.getString("firstName");
-                    String surname = object.getString("lasttName");
+    public void lunchPicker(View view) {
+        Intent i = new Intent(this, LunchPicker.class);
+        startActivity(i);
+    }
 
-                    data.add(name + " " + surname);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    return;
-                }
-
-            }
-            for (String row : data) {
-                String currentText = osebe.getText().toString();
-                osebe.setText(currentText + " \n\n" + row);
-            }
-
-        }
-
-    };
-    private Response.ErrorListener errorListener = new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            Log.d("REST error", error.getMessage());
-
-        }
-    };
 }
